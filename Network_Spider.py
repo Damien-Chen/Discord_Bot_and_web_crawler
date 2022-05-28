b77221f4e4b9247d53a305166aba9600baa126ac
Damien <81557462+Damien-Chen@users.noreply.github.com>
2022-05-29 07:31:04 +0800

    Update Network_Spider.py

--- Network_Spider.py
+++ Network_Spider.py
@@ -1,18 +1,46 @@
 from email.header import Header
 from encodings import utf_8
-from types import AsyncGeneratorType
 from urllib import response
 import urllib.request as req
 import requests
+import Global
+import time
+import discord
 from bs4 import BeautifulSoup
 
-url = "https://www.ptt.cc/bbs/PC_Shopping/index.html"
-Headers = {Your User Agent}
+Global.initialize()
 
+#Get titles
+url = "https://www.ptt.cc/bbs/PC_Shopping/index.html"
+Headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",}
 r = requests.get(url,headers = Headers)
 soup = BeautifulSoup(r.text, 'html.parser')
 titles = soup.find_all("div", class_="title")
 
+
+
+#Get the number of titles
+Global.list = []
+Global.link = []
+for title in titles:
+    if title.a != None:     
+        Global.list.append(title.a.string)
+        Global.link.append(title.a['href'])
+        time.sleep(0.1)
+        Global.num += 1
+
+
+#for i in range(len(Global.list)):
+  #  print(Global.list[Global.i])
+  # print(Global.link[Global.i])
+  #  Global.i += 1 
+  #  time.sleep(1)
+        
+
+#Return titles
 def PData(self):
-    return titles
+    for title in titles:
+        if title.a != None:
+            return (Global.list[Global.i],Global.link[Global.i])
+
     
